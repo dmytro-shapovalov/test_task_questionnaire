54813a965a88e8e2c99f17028b6143602f8a5f74
Dmytro Shapovalov <dmitry.shapovaloff@gmail.com>
2025-03-05 17:18:27 +0200

    feat: styles for accented background

--- src/components/Questionnaire/components/QuestionnaireScreen.tsx
+++ src/components/Questionnaire/components/QuestionnaireScreen.tsx
@@ -1,3 +1,7 @@
+import AppLogo from '../../../assets/app_logo.svg';
+import AppLogoInverted from '../../../assets/app_logo_inverted.svg';
+import BackArrow from '../../../assets/back_arrow.svg';
+import BackArrowInverted from '../../../assets/back_arrow_inverted.svg';
 import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
 import { questionnaireActions, questionnaireSelectors } from '../../../redux/questionnaire/slice';
 import { ScreenConfig } from '../types';
@@ -18,25 +22,46 @@ function QuestionnaireScreen({ config, onAnswer }: Props) {
     dispatch(questionnaireActions.previousScreen());
   }
 
-  const backgroundClassName = { default: 'bg-surface-1', accent: 'bg-(image:--accent-gradient)' }[
-    config.background
-  ];
-
-  const textColorClassName = config.background === 'accent' ? 'text-white' : '';
+  const { backgroundClassName, textClassName, backIconSrc, logoIconSrc } = {
+    default: {
+      backgroundClassName: 'bg-surface-1',
+      textClassName: 'text-font-main',
+      logoIconSrc: AppLogo,
+      backIconSrc: BackArrow,
+    },
+    accent: {
+      backgroundClassName: 'bg-(image:--accent-gradient)',
+      textClassName: 'text-font-inverted text-center',
+      logoIconSrc: AppLogoInverted,
+      backIconSrc: BackArrowInverted,
+    },
+  }[config.background];
 
   return (
     <div
       className={`flex flex-col items-center h-full w-full overflow-hidden ${backgroundClassName}`}
     >
-      <button hidden={isFirst} onClick={handleBack} className="bg-red-300">
-        Back
-      </button>
+      <div className="w-full h-14 items-center px-4 sm:px-0 grid grid-cols-3 justify-items-center">
+        <button
+          hidden={isFirst}
+          onClick={handleBack}
+          className="justify-self-start sm:justify-self-auto w-10 h-10"
+        >
+          <img src={backIconSrc} alt="Back arrow" />
+        </button>
+        <img
+          src={logoIconSrc}
+          alt="App logo. Stylized n for nebula"
+          className="col-start-2 col-end-2"
+        />
+      </div>
+
       <h1
-        className={`w-full sm:w-80 px-4 sm:px-0 text-2xl font-bold leading-7 ${textColorClassName}`}
+        className={`w-full sm:w-80 px-4 sm:px-0 text-2xl font-bold leading-7 mt-2 ${textClassName}`}
       >
         {config.title}
       </h1>
-      <h2 className={`w-full sm:w-80 px-4 sm:px-0 text-sm mb-2 ${textColorClassName}`}>
+      <h2 className={`w-full sm:w-80 px-4 sm:px-0 text-sm mb-2 leading-7 mt-5 ${textClassName}`}>
         {config.instruction}
       </h2>
       <Answers config={config} onAnswer={onAnswer} />
